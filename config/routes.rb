24ecaffd52a2c214ce7e9e6a996cb57ebfa1cdc2
naltun <nbaltunian@gmail.com>
2015-06-26 13:04:57 +0100

    Tab route block
    Tab route block 2 spaces in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-root to: 'ballad#home'
+	root to: 'ballad#home'
 
-get '/home', to: 'ballad#home'
-get '/about', to: 'ballad#about'
-get '/contact', to: 'ballad#contact'
-get '/signup', to: 'ballad#signup'
+	get '/home', to: 'ballad#home'
+	get '/about', to: 'ballad#about'
+	get '/contact', to: 'ballad#contact'
+	get '/signup', to: 'ballad#signup'
 end
