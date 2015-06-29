71b60c3e7a4a16f04125db4c4574cd4815e94fb5
udonokoh <udonokoh@gmail.com>
2015-06-29 18:06:22 +0100

    Wrote tests for Place model

---
+++ spec/models/place_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+describe Place do |
+	it "is valid with a name, latitude and longitude"
+	it "is invalid without a name"
+	it "is invalid without a latitude"
+	it "is invalid without a longitude"
+end|
+	
+end
