86df18d37aaca70f39d948f415230c4d96cf8ddb
udonokoh <udonokoh@gmail.com>
2015-07-07 14:35:28 +0100

    Created forms and views for song model, changes to place and composer views

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,6 +1,8 @@
 class SongsController < ApplicationController
+	before_action :find_song, only: [:show, :edit, :update, :destroy]
 	
 	def index
+		@songs = Song.all.order("created_at DESC")
 	end
 
 	def show
@@ -24,14 +26,22 @@ class SongsController < ApplicationController
 	end
 
 	def update
+		if @song.update(song_params)
+			redirect_to @song
+		else
+			render 'edit'
+		end
 	end
 
 	def destroy
+		@song.destroy
+		redirect_to root_path
 	end
 
 	private
 
 	def find_song
+		@song = Song.find(params[:id])
 	end
 
 	def song_params
