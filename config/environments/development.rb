94f523373df7be672d2d69d5d4ce2c3c43eebec2
udonokoh <udonokoh@gmail.com>
2015-07-23 11:55:08 +0100

    environments/devepoment and productions modifications

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,6 +44,6 @@ config.paperclip_defaults = {
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
   }
-
+}
 
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,5 +89,6 @@ Rails.application.configure do
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
   }
+}
   
 end
