4c580f785586aad10b54eae263368dbc2b3e533c
udonokoh <udonokoh@gmail.com>
2015-07-23 09:51:15 +0100

    Minor changes

--- app/models/poem.rb
+++ app/models/poem.rb
@@ -4,10 +4,10 @@ class Poem < ActiveRecord::Base
 	belongs_to :place
 	belongs_to :composer
 
+	has_attached_file :medialink
+	validates_attachment_content_type :medialink, :content_type => [ 'audio/mp3','audio/mpeg','video/mp4']
+
 	validates :title, presence: true
 	validates :subtitle, presence: true
 
-	has_attached_file :medialink
-	validates_attachment_presence :medialink
-	validates_attachment_content_type :medialink, :content_type => [ 'audio/mp3','audio/mpeg','video/mp4']
 end
--- db/migrate/20150708205109_add_end_user_id_to_poem.rb
+++ db/migrate/20150708205109_add_end_user_id_to_poem.rb
@@ -1,5 +1,5 @@
 class AddEndUserIdToPoem < ActiveRecord::Migration
   def change
-    add_column :poems, :end_user_id, :integer
+    add_column :poems, :end_user_id, :integer		
   end
 end
