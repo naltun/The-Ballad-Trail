890f6c86e1a690ef5cf7de8f0447af32961c3b9b
naltun <nbaltunian@gmail.com>
2015-07-29 22:42:23 +0100

    Update composer factory and spec

--- spec/models/composer_spec.rb
+++ spec/models/composer_spec.rb
@@ -2,34 +2,31 @@ require 'rails_helper'
 
 describe Composer do
 	it 'has a valid factory' do
-		expect(build(:composer)).to be_valid
+		expect(FactoryGirl.build(:composer)).to be_valid
 	end
 
 	it 'is invalid without a firstname (as fname) and lastname (as lname)' do
-		composer = Composer.new(fname: nil, lname: nil)
+		composer = FactoryGirl.build(:composer, fname: nil, lname: nil)
 		composer.valid?
 		expect(composer.valid?).to eq false
 	end
 
-	it "ins invalid without an end_user_id" do
-		composer = Composer.new(end_user_id: nil)
+	it "is invalid without an end_user_id" do
+		composer = FactoryGirl.build(:composer, end_user_id: nil)
 		composer.valid?
-		expect(composer.valid?).to eq false
+		expect(composer.valid? == false)
 	end
 	
 	it "is invalid without a composer type" do
-		composer = Composer.new(composer_type: nil)
+		composer = FactoryGirl.build(:composer, composer_type: nil)
 		composer.valid?
-		expect(composer.valid?).to eq false
+		expect(composer.valid? == false)
 	end
 
 	it "is invalid without a bio" do
-		composer = Composer.new(bio: nil)
+		composer = FactoryGirl.build(:composer, bio: nil)
 		composer.valid?
 		# Contains the following syntax as I like it better :)
 		expect(composer.valid? == false)
 	end
-
-#RSpec.describe Composer, :type => :model do
-  #pending "add some examples to (or delete) #{__FILE__}"
 end
