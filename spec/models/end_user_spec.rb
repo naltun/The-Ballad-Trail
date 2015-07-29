2b6570a41d44a1a48dbdb431e83b2f995d4b759a
naltun <nbaltunian@gmail.com>
2015-07-29 13:52:02 +0100

    Update EndUser model spec

--- spec/models/end_user_spec.rb
+++ spec/models/end_user_spec.rb
@@ -9,21 +9,21 @@ RSpec.describe EndUser, :type => :model do
 			password: 'password')
 		expect(end_user).to be_valid  	
   end
-  it "is invalid with an email" do
-  	end_user = EndUser.new(email: nil)
-  	end_user.valid?
-  	expect(end_user.errors[:email]).to include("Email can't be blank")
+  it "is invalid without an email" do
+    end_user = EndUser.new(email: nil)
+    end_user.valid?
+    expect(end_user.valid? == false)
   end
   it "is invalid without a password" do
   	end_user = EndUser.new(password: nil)
   	end_user.valid?
-  	expect(end_user.errors[:password]).to include("Password can't be blank")
+  	expect(end_user.valid? == false)
   end
 
   it "is invalid with a duplicate email address" do
-  	EndUser.create(email: 'example@example.com', password: 'password')
-  	end_user = EndUser.new(email: 'example@example.com', password: 'password')
-  	end_user.valid?
-  	expect(end_user.errors[:email]).to include("Email has already been taken")
+    EndUser.create(email: 'example@example.com', password: 'password')
+    end_user = EndUser.new(email: 'example@example.com', password: 'mypassword')
+    end_user.valid?
+    expect(end_user.valid? == false)
   end
 end
