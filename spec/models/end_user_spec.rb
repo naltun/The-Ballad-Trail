a4faf57aaa36560c4fbafda55cff78a48e4c9f75
naltun <nbaltunian@gmail.com>
2015-07-29 22:47:59 +0100

    Update end_user factory and spec!

--- spec/models/end_user_spec.rb
+++ spec/models/end_user_spec.rb
@@ -3,19 +3,24 @@ require 'rails_helper'
 RSpec.describe EndUser, :type => :model do
   pending "add some examples to (or delete) #{__FILE__}"
 
+  it "has a valid factory" do
+    expect(FactoryGirl.build(:end_user)).to be_valid
+  end
+
   it "is valid with an email and password" do
 		end_user = EndUser.new(
 			email: 'example@example.com',
 			password: 'password')
 		expect(end_user).to be_valid  	
   end
+
   it "is invalid without an email" do
-    end_user = EndUser.new(email: nil)
+    end_user = FactoryGirl.build(:end_user, email: nil)
     end_user.valid?
     expect(end_user.valid?).to eq false
   end
   it "is invalid without a password" do
-  	end_user = EndUser.new(password: nil)
+    end_user = FactoryGirl.build(:end_user, password: nil)
   	end_user.valid?
   	expect(end_user.valid?).to eq false
   end
