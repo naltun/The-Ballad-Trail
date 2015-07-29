51998b83bcf6c6d9fe09059d2de99fd9373d7b35
naltun <nbaltunian@gmail.com>
2015-07-29 22:32:59 +0100

    Update admin spec and add factory
    Update admin spec with best practices and also fix admin factory

--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -3,15 +3,22 @@ require 'rails_helper'
 RSpec.describe Admin, :type => :model do
   pending "add some examples to (or delete) #{__FILE__}"
 
+  it "has a valid factory" do
+    expect(FactoryGirl.build(:admin)).to be_valid
+  end
+
   it "is valid with an email and password" do
-  	admin = Admin.new(email: 'admin@balladtrail.com', password: 'theadmin')
-  	admin.valid?
-  	expect(admin.valid?)
+    admin = Admin.new(
+      email: 'tbt_admin1@balladtrail.com',
+      password: 'password'
+    )
+    admin.valid?
+    expect(admin).to be_valid
   end
 
 	it "is invalid without an email and password" do
-		admin = Admin.new(email: nil, password: nil)
-		admin.valid?
-		expect(admin.valid?).to eq false
+    admin = FactoryGirl.build(:admin, email: nil, password: nil)
+    admin.valid?
+    expect(admin.valid?).to eq false
 	end
 end
