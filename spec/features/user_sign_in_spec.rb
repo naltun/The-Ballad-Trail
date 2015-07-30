0f5674e22d08cd6baa4922a78b5a7ef9deadb5fd
Maria3112 <m-nikole@hotmail.com>
2015-07-30 19:18:41 +0100

    Change testing user sign in

--- spec/features/user_sign_in_spec.rb
+++ spec/features/user_sign_in_spec.rb
@@ -1,18 +1,10 @@
 require "spec_helper"
 require 'rails_helper'
 
-describe "user sign in" do
-  it "allows users to sign in after they have registered" do
-    user = EndUser.create(:email    => "email@example.com",
-                       :password => "password")
-
-    visit "/end_users/sign_in"
-
-    fill_in "Email",    :with => "email@example.com"
-    fill_in "Password", :with => "password"
-
-    click_button "Log in"
-
-    page.should have_content( "HOME MENU Contact us About Logged in as email@example.com. Edit profile Logout All Places All Composers All Poems All Songs this is menu Signed in successfully.")
+When /I sign in/ do
+  within("/end_users/sign_in") do
+    fill_in 'Email', :with => 'user@example.com'
+    fill_in 'Password', :with => 'password'
   end
+  click_button 'Sign in'
 end
