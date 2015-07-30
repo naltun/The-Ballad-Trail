a4bc87ae1d7f10c6b67d64ed6c7da76db98d1a17
udonokoh <udonokoh@gmail.com>
2015-07-30 13:59:52 +0100

    Migrated down the paperclip image attribute and deleted migration file

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150728154014) do
+ActiveRecord::Schema.define(version: 20150723092300) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,13 +67,9 @@ ActiveRecord::Schema.define(version: 20150728154014) do
     t.decimal  "latitude"
     t.decimal  "longitude"
     t.string   "region"
-    t.datetime "created_at",         null: false
-    t.datetime "updated_at",         null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.integer  "end_user_id"
-    t.string   "image_file_name"
-    t.string   "image_content_type"
-    t.integer  "image_file_size"
-    t.datetime "image_updated_at"
   end
 
   create_table "poems", force: :cascade do |t|
