6d5db909903ea384f1634fd2698c9d452d8a0cc6
Maria3112 <m-nikole@hotmail.com>
2015-07-31 12:10:05 +0100

    Tests for admin sign out user sign out

--- spec/features/user_sign_out_spec.rb
+++ spec/features/user_sign_out_spec.rb
@@ -1,12 +1,11 @@
-require "spec_helper"
-require 'rails_helper'
+
 
 describe "user sign out " do
   it "allows user to sign out " do
+    visit "/end_users/sign_in"
     
 
-    visit "/"
-
-   page.should have_content( "HOME MENU Contact us About Admin Controls Sign Up! Login All Places All Composers All Poems All Songs this is menu")
+   page.should have_content("Login" )
   end
 end
+
